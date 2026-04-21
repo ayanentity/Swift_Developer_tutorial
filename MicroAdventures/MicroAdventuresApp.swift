174b14ff83276a321f65c0d0ddd8eaaf3d72e4cc
ayanentity <ayanentity154@gmail.com>
2026-04-22 07:09:06 +0900

    Initial Commit

---
+++ MicroAdventures/MicroAdventuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicroAdventuresApp.swift
+//  MicroAdventures
+//
+//  Created by 仲里絢音 on 2026/04/22.
+//
+
+import SwiftUI
+
+@main
+struct MicroAdventuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
