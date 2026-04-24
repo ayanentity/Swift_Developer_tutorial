b9ee76ea7e8efed3c216b1495876472b505a9172
ayanentity <ayanentity154@gmail.com>
2026-04-24 17:03:00 +0900

    インスタンスの作成と装飾子の順序のチュートリアル

--- MicroAdventures/ExploreView.swift
+++ MicroAdventures/ExploreView.swift
@@ -8,7 +8,60 @@ import SwiftUI
 
 struct ExploreView: View {
     var body: some View {
-        Text("Explore!! Hello, I am ayanentity")
+        VStack{
+            Text("Hello")
+                .padding()
+                .background(Color.blue, in: RoundedRectangle(cornerRadius: 16))
+            Text("I am ayanentity")
+                .padding()
+                .background(Color.red, in:RoundedRectangle(cornerRadius: 16))
+        }
+        HStack{
+            DayForecast(day: "Mon", isRany:false, high: 20, low: 10)
+            DayForecast(day: "Tue", isRany:true, high: 10, low: -2)
+        }
+        .padding()
+    }
+}
+
+
+struct DayForecast: View {
+    let day: String
+    let isRany: Bool
+    let high: Int
+    let low: Int
+    
+    var iconName: String{
+        if isRany{
+            return "cloud.rain.fill"
+        } else{
+            return "sun.max.fill"
+        }
+    }
+     
+    var iconColor: Color{
+        if isRany{
+            return Color.blue
+        } else {
+            return Color.orange
+        }
+    }
+    
+    
+    var body: some View {
+        VStack {
+            Text(day)
+                .font(Font.headline)
+            Image(systemName: iconName)
+                .foregroundStyle(iconColor)
+                .font(Font.largeTitle)
+                .padding(4)
+            Text("High: \(high)")
+                .fontWeight(Font.Weight.semibold)
+            Text("low: \(low)")
+                .fontWeight(Font.Weight.medium)
+        }
+        .padding()
     }
 }
 
