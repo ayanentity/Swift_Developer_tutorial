92a7fba7bc313556672502120bd1801e61fa60b4
ayanentity <ayanentity154@gmail.com>
2026-04-28 07:45:38 +0900

    Model data with custom types　tutorial

--- ScoreKeeper/ScoreKeeper/ContentView.swift
+++ ScoreKeeper/ScoreKeeper/ContentView.swift
@@ -8,16 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var players: [Player] = [ //独自のデータモデル
+        Player(name: "Elisha", score: 0),
+        Player(name: "Ayane", score: 0),
+        Player(name: "Jasmine", score: 0),
+        
+    ]
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack (alignment: .leading){ // 左寄せ指定
+            Text("Score Keeper")
+                .font(.title)
+                .bold()
+                .padding(.bottom)
+                
+            Grid {
+                GridRow{
+                    Text("Player")
+                        .gridColumnAlignment(.leading)
+                    Text("Score")
+                }
+                .font(.headline)
+                
+                
+                ForEach($players){ $player in
+                    GridRow{ //コンテンツをすべて1行に
+                        TextField("Name", text: $player.name) //nameはテキストフィールドが空の際に表示されるテキスト
+                        Stepper("\(player.score)" , value: $player.score)
+                    }
+                }
+            }
+            .padding(.vertical)
+            
+            Button("Add Player", systemImage: "plus"){
+                players.append(Player(name: "", score: 0))
+            }
+            
+            Spacer()
         }
         .padding()
     }
 }
+
 
 #Preview {
     ContentView()
