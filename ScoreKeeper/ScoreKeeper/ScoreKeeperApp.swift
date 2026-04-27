231d0d1ba5e6e115663a8d79e284f1864311d60a
ayanentity <ayanentity154@gmail.com>
2026-04-27 21:19:55 +0900

    ScoreKeeperApp フォルダの作成

---
+++ ScoreKeeper/ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by 仲里絢音 on 2026/04/27.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
