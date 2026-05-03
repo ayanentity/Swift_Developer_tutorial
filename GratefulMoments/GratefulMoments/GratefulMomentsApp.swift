8935804d6d3f9407a61b07742a84ba61a8e49eb2
ayanentity <ayanentity154@gmail.com>
2026-05-03 09:20:43 +0900

    GratefulMomentsAppのプロジェクトを作成

---
+++ GratefulMoments/GratefulMoments/GratefulMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by 仲里絢音 on 2026/05/03.
+//
+
+import SwiftUI
+
+@main
+struct GratefulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
