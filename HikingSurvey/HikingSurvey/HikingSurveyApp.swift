fc6f1b48aee25a31bcfc142730b055bcf0441afc
ayanentity <ayanentity154@gmail.com>
2026-05-05 08:11:35 +0900

    機械学習チュートリアル用Appの作成

---
+++ HikingSurvey/HikingSurvey/HikingSurveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingSurveyApp.swift
+//  HikingSurvey
+//
+//  Created by 仲里絢音 on 2026/05/05.
+//
+
+import SwiftUI
+
+@main
+struct HikingSurveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
